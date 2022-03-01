460b5dad6afbad2da55956e98201653a0242819f
Anurag Pandey <arushpandey123buy@gmail.com>
2022-03-02 05:28:50 +0530

    frontend starts

--- carzone/settings.py
+++ carzone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR / 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'carzone/static'),
+]
 
-# Default primary key field type
+
+# Default primary key field  type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
