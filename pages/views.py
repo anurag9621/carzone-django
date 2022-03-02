f5c39e24e52a6fe3121046990ce2439648309854
Anurag Pandey <arushpandey123buy@gmail.com>
2022-03-03 01:36:26 +0530

    frontend

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "pages/index.html")
+
+def about(request):
+    return render(request,'pages/about.html')
+
+def services(request):
+    return render(request,'pages/services.html')
+
+def contact(request):
+    return render(request,'pages/contact.html')
