9c691dc844c889b69ce41a51df6c32fd696ec193
Anurag Pandey <arushpandey123buy@gmail.com>
2022-03-08 05:07:08 +0530

    cars

--- carzone/settings.py
+++ carzone/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- carzone/urls.py
+++ carzone/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    path('cars/', include('cars.urls'))
+
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
