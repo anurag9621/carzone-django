feffe794b2f122f9bc504096740dc647559ff232
Anurag Pandey <arushpandey123buy@gmail.com>
2022-03-10 03:46:25 +0530

    kk

--- cars/models.py
+++ cars/models.py
@@ -118,5 +118,5 @@ class Car(models.Model):
     created_date = models.DateTimeField(default=datetime.now,blank=True)
 
 
-def __str__(self):
-    return self.car_title
+    def __str__(self):
+        return self.car_title
