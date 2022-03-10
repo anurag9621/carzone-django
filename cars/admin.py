8220f8a87ab0e937397bbb571bd640d6ddb84fe6
Anurag Pandey <arushpandey123buy@gmail.com>
2022-03-10 05:36:23 +0530

    admin page edit done

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-
 from cars.models import Car
+from django.utils.html import format_html
+
 
 # Register your models here.
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+
+    def thumbnail(self, object):
+        return format_html(
+            '<img src="{}" width="40" style="border-radius:50px;" /> '.format(
+                object.car_photo.url))
+
+    thumbnail.short_description = "Car Image"
+
+    list_display = ('id','thumbnail','car_title', 'color', 'model', 'year', 'body_style',
+                    'fuel_type', 'is_featured')
+
+    list_display_links=(
+        "id",
+        "thumbnail",
+        "car_title"
+    )
+    list_editable = ('is_featured',)
+    search_fields=('id','car_title','city','state','model','body_style','fuel_type',)
+    list_filter = ( 'city', 'model', 'body_style', 'fuel_type')
+
+admin.site.register(Car, CarAdmin)
