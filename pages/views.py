d3b833409a02c2a1db662dbf649a2552892097c0
Anurag Pandey <arushpandey123buy@gmail.com>
2022-03-10 06:41:49 +0530

    add cars to home page

--- pages/views.py
+++ pages/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
+from cars.models import Car
 
 from pages.models import Team
 
+
 # Create your views here.
 def home(request):
-    teams= Team.objects.all()
-    data={
+    teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(
+        is_featured=True)
+    data = {
         'teams': teams,
-
+        'featured_cars': featured_cars,
     }
-    return render(request, "pages/index.html",data)
+    return render(request, "pages/index.html", data)
+
 
 def about(request):
-    teams=Team.objects.all()
-    data={
-        'teams':teams,
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
     }
-    return render(request,'pages/about.html', data)
+    return render(request, 'pages/about.html', data)
+
 
 def services(request):
-    return render(request,'pages/services.html')
+    return render(request, 'pages/services.html')
+
 
 def contact(request):
-    return render(request,'pages/contact.html')
+    return render(request, 'pages/contact.html')
